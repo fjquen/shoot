4d754bb11711cd039bbedd3b67a2bbbce3fe6fca
fjquen <fjquen@gmail.com>
2023-08-28 21:37:35 +0200

    ajout image player

--- premier_jeu/main.rb
+++ premier_jeu/main.rb
@@ -2,16 +2,25 @@ require 'gosu'
 
 
 class Main < Gosu::Window
-    
+    PLAYER = "§"
+    ENNEMY = "°"
+    VOID = " "
+    WIDTH_TILE = 55
+    HEIGHT_TILE = 55
+    WIDTH_TILE_PLAYER = 65
+    HEIGHT_TILE_PLAYER = 65
+
     def initialize
         super 640,480
         self.caption = "premier jeu"
-        @area_combat = [["°","°","°","°","°","°","°","°","°",],
+        @area_combat = [[" "," "," "," "," "," "," "," "," ",],
+                        [" ","°"," "," "," "," ","°"," "," ",],
+                        [" "," "," ","°"," "," "," "," "," ",],
+                        [" ","°"," "," "," "," "," "," "," ",],
                         [" "," "," "," "," "," "," "," "," ",],
                         [" "," "," "," "," "," "," "," "," ",],
-                        [" "," "," "," "," "," "," "," "," ",],
-                        [" "," "," "," "," "," "," "," "," ",],
-                        [" "," "," "," ","§"," "," "," "," ",]]
+                        ["§"," "," "," "," "," "," "," "," ",]]
+        @player = Gosu::Image.new("media/playerShip1_blue.png")
     end
     
     def update
@@ -19,7 +28,19 @@ class Main < Gosu::Window
     end
    
     def draw
-        
+       draw_area
+    end
+
+    def draw_area
+        @area_combat.each_index do |y|
+            @area_combat[y].each_index do |x|
+                if @area_combat[y][x] == VOID
+                    Gosu.draw_rect(x*WIDTH_TILE, y*HEIGHT_TILE, WIDTH_TILE, HEIGHT_TILE,Gosu::Color::BLACK)
+                elsif @area_combat[y][x] == PLAYER
+                    @player.draw(x*WIDTH_TILE_PLAYER, y*HEIGHT_TILE_PLAYER)
+                end
+            end
+        end
     end
 end
 Main.new.show
