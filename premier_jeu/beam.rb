af2f989e33fb0908a9a32b73b06ef25bb30c692f
fjquen <fjquen@gmail.com>
2023-08-31 11:18:22 +0200

    mise en place de la direction du laser

--- premier_jeu/beam.rb
+++ premier_jeu/beam.rb
@@ -1,6 +1,11 @@
 module Beam
     BEAM = "|"
     def move_beam
-        puts "yo"
+        pos_row_player = @area1.flatten.index(player)
+        pos_col_player = @area1.first.size
+        row_player = pos_row_player / pos_col_player
+        col_player = pos_row_player % pos_col_player
+        @y = row_player
+        @y -= 1
     end
 end
