a6a69e8fb62156415dd528b508c843318fa86ba1
fjquen <fjquen@gmail.com>
2023-09-06 17:08:31 +0200

    mouvement de l'ennemi

--- premier_jeu/move_game.rb
+++ premier_jeu/move_game.rb
@@ -36,4 +36,36 @@ module Move_game
         return if @area_combat[row_player][@x] < VOID
         @area_combat[row_player][col_player],@area_combat[row_player][@x] = @area_combat[row_player][@x],@area_combat[row_player][col_player]
     end
+
+    def move_right_ennemy()
+        pos_row_ennemy = @area_combat.flatten.index(ENNEMY)
+        pos_col_ennemy = @area_combat.first.size
+        row_ennemy = pos_row_ennemy / pos_col_ennemy
+        col_ennemy = pos_row_ennemy % pos_col_ennemy
+        @x = col_ennemy
+        limit_x = pos_col_ennemy - 1
+        if @x < limit_x
+            @x += 1
+        else
+            @x = limit_x
+        end
+        return if @area_combat[row_ennemy][@x] ==  @area_combat[row_ennemy][col_ennemy]
+        @area_combat[row_ennemy][col_ennemy],@area_combat[row_ennemy][@x] = @area_combat[row_ennemy][@x],@area_combat[row_ennemy][col_ennemy]
+    end
+
+    def move_left_ennemy()
+        pos_row_ennemy = @area_combat.flatten.index(ENNEMY)
+        pos_col_ennemy = @area_combat.first.size
+        row_ennemy = pos_row_ennemy / pos_col_ennemy
+        col_ennemy = pos_row_ennemy % pos_col_ennemy
+        @x = col_ennemy
+        limit_x = 0
+        if @x > limit_x
+            @x -= 1
+        else
+            @x = limit_x
+        end
+        return if @area_combat[row_ennemy][@x] < VOID
+        @area_combat[row_ennemy][col_ennemy],@area_combat[row_ennemy][@x] = @area_combat[row_ennemy][@x],@area_combat[row_ennemy][col_ennemy]
+    end
 end
