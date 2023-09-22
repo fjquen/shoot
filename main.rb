b06a57dcd263e08227039d73cfe2f84b53260fa4
fjquen <fjquen@gmail.com>
2023-09-22 13:47:46 +0200

    modif random ennemy

--- main.rb
+++ main.rb
@@ -36,12 +36,16 @@ class Main < Gosu::Window
     def update
        @number_ennemy = @area_combat.flatten.select { |n| n == "°" }.length
        numRandex = @area_combat.flatten.length - 1
-       index_tab =@tab[rand(numRandex)]
-       #puts "#{index_tab["x"]} #{index_tab["y"]}"
-
-       if @number_ennemy < 10
-        @area_combat[index_tab["y"]][index_tab["x"]] = ENNEMY
+       if @number_ennemy == 0
+        index_tab =@tab[rand(0..numRandex)]
+        puts index_tab["y"]
+        if @number_ennemy < 10 && index_tab["y"] <= 6
+            @area_combat[index_tab["y"]][index_tab["x"]] = ENNEMY
+        end
        end
+       #puts "#{index_tab["x"]} #{index_tab["y"]}"
+       
+       
        #@area_combat[6][1]=ENNEMY
        #voir tuto pour générer de manière aléatoire les vaisseaux
     #    if Gosu.milliseconds % 20 == 0
