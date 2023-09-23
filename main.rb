ed24a7c29d15094f4dfd2b6a2b2a30b4500cd8a3
fjquen <fjquen@gmail.com>
2023-09-23 19:55:17 +0200

    mise en commentaire

--- main.rb
+++ main.rb
@@ -35,8 +35,8 @@ class Main < Gosu::Window
     
     def update
        @number_ennemy = @area_combat.flatten.select { |n| n == "°" }.length
-       @y += 1
-       puts @area_combat[@y][@tab[0]["x"]]
+    #    @y += 1
+    #    puts @area_combat[@y][@tab[0]["x"]]
     #    return if @area_combat[@tab[0]["y"]][@tab[0]["x"]] < VOID && @y<6
     #    @y += 1 
     #    puts @area_combat[@y][@tab[0]["x"]]
