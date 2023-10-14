b4ef150b2b3d3a34c1e5d38f8717045f0a0e2627
fjquen <fjquen@gmail.com>
2023-10-14 17:27:23 +0200

    indentation

--- beam.rb
+++ beam.rb
@@ -11,33 +11,33 @@ module Beam
         col_player = pos_row_player % pos_col_player
         @y = row_player
         @y -= 1
-        @area_combat.each_index do |y|
-            @area_combat[y].each_index do |x|
-                if @area_combat[@y][col_player] == VOID
-                    if col_player==x && y==@y
-                        @area_combat[y][x] = BEAM
-                            for y in 0..@area_combat.length
-                                next if @area_combat[@y][col_player] == VOID
-                                    y-= 1
-                                    @area_combat[y][x] = BEAM
-                                    player = @area_combat.length - 1
-                                    @area_combat[player][col_player] = PLAYER
-                            end
-                    end
-                elsif @area_combat[@y][col_player] == ENNEMY
-                    if col_player==x && y==@y
-                        @area_combat[y][x] = BEAM
-                            for y in 0..@area_combat.length
-                                next if @area_combat[@y][col_player] == ENNEMY
-                                    y-= 1
-                                    @area_combat[y][x] = BEAM
-                                    player = @area_combat.length - 1
-                                    @area_combat[player][col_player] = PLAYER
-                            end
+            @area_combat.each_index do |y|
+                @area_combat[y].each_index do |x|
+                    if @area_combat[@y][col_player] == VOID
+                        if col_player==x && y==@y
+                            @area_combat[y][x] = BEAM
+                                for y in 0..@area_combat.length
+                                    next if @area_combat[@y][col_player] == VOID
+                                        y-= 1
+                                        @area_combat[y][x] = BEAM
+                                        player = @area_combat.length - 1
+                                        @area_combat[player][col_player] = PLAYER
+                                end
+                        end
+                    elsif @area_combat[@y][col_player] == ENNEMY
+                        if col_player==x && y==@y
+                            @area_combat[y][x] = BEAM
+                                for y in 0..@area_combat.length
+                                    next if @area_combat[@y][col_player] == ENNEMY
+                                        y-= 1
+                                        @area_combat[y][x] = BEAM
+                                        player = @area_combat.length - 1
+                                        @area_combat[player][col_player] = PLAYER
+                                end
+                        end
                     end
                 end
             end
         end
     end
-  end
 end
