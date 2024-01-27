6f467753f8ea776414b32c6c83be2758046ad77f
fjquen <fjquen@gmail.com>
2024-01-27 16:01:04 +0100

    changement de nom de fonction

--- beam.rb
+++ beam.rb
@@ -44,7 +44,7 @@ module Beam
         end
     end
 
-    def slow_beam()
+    def shot_beam()
         player = @arr.flatten.select { |n| n == PLAYER }.length
         if !@arr.flatten.index(PLAYER).nil? && !@arr.first.size.nil?
             col_player = @arr.flatten.index(PLAYER) % @arr.first.size
